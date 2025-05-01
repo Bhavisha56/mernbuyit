c98771ef4ebe495e51758f6a871d0b3a3be57cdf
Bhavisha56 <97666630+Bhavisha56@users.noreply.github.com>
2025-05-01 15:06:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 
 
 const corsOption={
-   origin:"https://mernbuyit-76v9.vercel.app",
+   origin:"https://mernbuyit-bugj.vercel.app",
    methods:"GET,POST,PUT,PATCH,DELETE,HEAD",
    credentials:true
 }
